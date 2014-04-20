05927d746b75b755294a04acc2cccc5fc7762d6f
Sam-Mauris Yong <mauris@hotmail.sg>
2014-04-20 18:23:22 +0800

    updated defaults

--- src/config/config.php
+++ src/config/config.php
@@ -2,14 +2,13 @@
 
 return array(
 
-    'id' => 'captcha',
-    'fontsizes' => array(14, 15, 16, 17, 18),
-    'length' => 5,
+    'fontsizes' => array(14, 15, 16, 17, 18, 19, 20),
+    'length' => 6,
     'width' => 120,
     'height' => 30,
     'space' => 20,
     'colors' => array('128,23,23', '128,23,22', '33,67,87', '67,70,83', '31,56,163', '48,109,22', '165,42,166', '18,95,98', '213,99,8'),
     'sensitive' => false, // case sensitive (params: true, false)
-    'quality' => 80 // image quality
+    'quality' => 75 // image quality
 
 );
