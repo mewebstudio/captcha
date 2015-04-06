f709eef80686d8c7fe8ca1dd82aa726d419d9f01
Muharrem <me@mewebstudio.com>
2015-04-06 10:37:41 +0300

    fix tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,18 +2,18 @@
 
 class TestCase extends \Illuminate\Foundation\Testing\TestCase {
 
-	/**
-	 * Creates the application.
-	 *
-	 * @return \Illuminate\Foundation\Application
-	 */
-	public function createApplication()
-	{
-		$app = require __DIR__.'/../bootstrap/app.php';
+    /**
+     * Creates the application.
+     *
+     * @return \Illuminate\Foundation\Application
+     */
+    public function createApplication()
+    {
+        $app = require __DIR__.'/../../../../bootstrap/app.php';
 
-		$app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
+        $app->make('Illuminate\Contracts\Console\Kernel')->bootstrap();
 
-		return $app;
-	}
+        return $app;
+    }
 
 }
