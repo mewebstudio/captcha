8ab1a0e1e135f6e2b8ca64ac2025d7036293acbf
Che Kun <kun.che@yoozi.cn>
2015-05-13 15:20:21 +0800

    Reset fonts array index

--- src/Captcha.php
+++ src/Captcha.php
@@ -199,6 +199,7 @@ class Captcha
     {
         $this->backgrounds = $this->files->files(__DIR__ . '/../assets/backgrounds');
         $this->fonts = $this->files->files(__DIR__ . '/../assets/fonts');
+        $this->fonts = array_values($this->fonts); //reset fonts array index
 
         $this->configure($config);
 
