9e5f8a677d9701b79f32a3f388f599428d3844ff
Zadolux <zadolux@inscomers.net>
2020-08-10 11:36:48 +0200

    Fix stateless Captcha flaws (add cache, expiration time)

--- src/Captcha.php
+++ src/Captcha.php
@@ -25,6 +25,7 @@ use Intervention\Image\Image;
 use Intervention\Image\ImageManager;
 use Illuminate\Session\Store as Session;
 use Illuminate\Support\HtmlString;
+use Illuminate\Support\Facades\Cache;
 
 /**
  * Class Captcha
@@ -286,6 +287,10 @@ class Captcha
             $this->image->blur($this->blur);
         }
 
+        if ($api) {
+            Cache::put('captcha_record_' . $generator['key'], $generator['value'], $this->expire);
+        }
+
         return $api ? [
             'sensitive' => $generator['sensitive'],
             'key' => $generator['key'],
@@ -477,6 +482,10 @@ class Captcha
      */
     public function check_api($value, $key): bool
     {
+        if (!Cache::pull('captcha_record_' . $key)) {
+            return false;
+        }
+
         return $this->hasher->check($value, $key);
     }
 
