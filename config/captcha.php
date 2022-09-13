a7cbfea835dd1dd02dde18634ff03a8751ea6d4a
Relax Dev <relaxdev@gmail.com>
2022-09-13 05:41:36 +0430

    feature: define 'disable' index in config with default value

--- config/captcha.php
+++ config/captcha.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+    'disable' => env('CAPTCHA_DISABLE', !str_contains(env('APP_ENV', 'local'), 'prod')),
     'characters' => ['2', '3', '4', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'm', 'n', 'p', 'q', 'r', 't', 'u', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'M', 'N', 'P', 'Q', 'R', 'T', 'U', 'X', 'Y', 'Z'],
     'default' => [
         'length' => 9,
