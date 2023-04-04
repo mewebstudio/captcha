30481dee230ea1c8f9a36c28efb8c2a3db59d0c0
Mohamed Attar <93054654+MohammedElattar@users.noreply.github.com>
2023-04-04 03:18:03 +0200

    Update CaptchaServiceProvider.php

--- src/CaptchaServiceProvider.php
+++ src/CaptchaServiceProvider.php
@@ -25,20 +25,22 @@ class CaptchaServiceProvider extends ServiceProvider
         ], 'config');
 
         // HTTP routing
-        if (strpos($this->app->version(), 'Lumen') !== false) {
-            /* @var Router $router */
-            $router = $this->app;
-            $router->get('captcha[/api/{config}]', 'Mews\Captcha\LumenCaptchaController@getCaptchaApi');
-            $router->get('captcha[/{config}]', 'Mews\Captcha\LumenCaptchaController@getCaptcha');
-        } else {
-            /* @var Router $router */
-            $router = $this->app['router'];
-            if ((double)$this->app->version() >= 5.2) {
-                $router->get('captcha/api/{config?}', '\Mews\Captcha\CaptchaController@getCaptchaApi')->middleware('web');
-                $router->get('captcha/{config?}', '\Mews\Captcha\CaptchaController@getCaptcha')->middleware('web');
+        if(!config('captcha.disable')){
+            if (strpos($this->app->version(), 'Lumen') !== false) {
+                /* @var Router $router */
+                $router = $this->app;
+                $router->get('captcha[/api/{config}]', 'Mews\Captcha\LumenCaptchaController@getCaptchaApi');
+                $router->get('captcha[/{config}]', 'Mews\Captcha\LumenCaptchaController@getCaptcha');
             } else {
-                $router->get('captcha/api/{config?}', '\Mews\Captcha\CaptchaController@getCaptchaApi');
-                $router->get('captcha/{config?}', '\Mews\Captcha\CaptchaController@getCaptcha');
+                /* @var Router $router */
+                $router = $this->app['router'];
+                if ((double)$this->app->version() >= 5.2) {
+                    $router->get('captcha/api/{config?}', '\Mews\Captcha\CaptchaController@getCaptchaApi')->middleware('web');
+                    $router->get('captcha/{config?}', '\Mews\Captcha\CaptchaController@getCaptcha')->middleware('web');
+                } else {
+                    $router->get('captcha/api/{config?}', '\Mews\Captcha\CaptchaController@getCaptchaApi');
+                    $router->get('captcha/{config?}', '\Mews\Captcha\CaptchaController@getCaptcha');
+                }
             }
         }
 
