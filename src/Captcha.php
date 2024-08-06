81754edd74d16f7c785e4da36bdf9c8f125a676f
Will Browning <20662079+willbrowningme@users.noreply.github.com>
2024-08-06 22:35:51 +0100

    Fixed Response duplicate declaration in Captcha.php
    use Illuminate\Http\Response; is declared twice which causes errors.

--- src/Captcha.php
+++ src/Captcha.php
@@ -29,7 +29,6 @@ use Illuminate\Session\Store as Session;
 use Illuminate\Support\HtmlString;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Crypt;
-use Illuminate\Http\Response;
 
 
 /**
